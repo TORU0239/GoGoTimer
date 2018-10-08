fc88f6fce39793abe6dd19cb6e51461b67833ad3
Toru Choi <toru.choi@11street.my>
2018-10-08 19:21:50 +0800

    open activity from pendingintent

--- app/src/main/java/my/com/toru/gogotimer/service/AlarmReceiver.kt
+++ app/src/main/java/my/com/toru/gogotimer/service/AlarmReceiver.kt
@@ -1,6 +1,7 @@
 package my.com.toru.gogotimer.service
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -9,6 +10,7 @@ import my.com.toru.gogotimer.R
 import my.com.toru.gogotimer.app.GoGoTimerApp
 import my.com.toru.gogotimer.database.AppDatabase
 import my.com.toru.gogotimer.model.TimerHistoryData
+import my.com.toru.gogotimer.ui.main.MainActivity
 import my.com.toru.gogotimer.util.CONST_CHANNEL_NAME
 
 class AlarmReceiver:BroadcastReceiver() {
@@ -37,10 +39,13 @@ class AlarmReceiver:BroadcastReceiver() {
 
     private fun sendNotification(context:Context){
         val notification = NotificationCompat.Builder(context, CONST_CHANNEL_NAME)
+        val intent = Intent(context , MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pendingIntent = PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         val noti = notification.setContentTitle("TEST")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentText("TRIGGERED!!!")
                 .setAutoCancel(true)
+                .setFullScreenIntent(pendingIntent, true)
                 .build()
         val notiManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notiManager.notify(1024, noti)
